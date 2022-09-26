acf0f0ec904482239572e9cc5afe0684a28c93f8
Sagar Salvi <sagarsalvi518@gmail.com>
2022-09-25 23:37:20 -0700

    Provider Script

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  access_key = ""
+  secret_key = ""
+}
+
